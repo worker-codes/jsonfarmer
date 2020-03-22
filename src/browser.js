00091cf7c8623bf1ff67839a869603955b584894
Dandre Allen <dandrefallen@gmail.com>
2020-03-22 16:43:30 -0400

    Add Query Valiadtion

--- src/browser.js
+++ src/browser.js
@@ -2,12 +2,25 @@ let gql = require('graphql-tag');
 let graphql = require('graphql-anywhere').default;
 let resolver = require('./resolver');
 let func = require('./func');
+
+let { parse } = require('graphql')
+
+function valiadteQuery(value) {
+    try {
+        let ast = parse(value);
+        return true
+    } catch (error) {
+        throw ""+ error
+    }
+}
+
 /**
  * Generate the Seeded Json
  * @param query - graphql gql
  * @returns {any}
  */
 function generate( query ) {
+    valiadteQuery(query)
     return graphql(resolver, gql`${query}` );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,26 @@ let resolver = require('./resolver');
 let mkdirp = require('mkdirp');
 let fs = require('fs');
 let path = require('path');
+let { parse } = require('graphql')
+let logSyntaxError = require('log-syntax-error')
 
-console.log(resolver);
+function valiadteQuery(value) {
+    try {
+        let ast = parse(value);
+        return true
+    } catch (error) {
+
+        error.locations
+        error.locations.forEach(location => {
+            var block = logSyntaxError( value, location.line, location.column-1 );
+            console.error( block );
+        });
+        
+        // console.error(error.message)
+        throw new Error(error.message)
+    }
+
+}
 
 /**
  * Generate the Seeded Json
@@ -13,6 +31,7 @@ console.log(resolver);
  * @returns {any}
  */
 function generate( query ) {
+    valiadteQuery(query)
     return graphql(resolver, gql`${query}` );
 }
 
@@ -22,6 +41,7 @@ function generate( query ) {
  * @param saveDir - path to save file
  */
 function save(query, saveDir) {
+    valiadteQuery(query)
     let data = graphql(resolver, gql`${query}` );
     let dir = path.dirname(saveDir);
 
@@ -34,7 +54,7 @@ function save(query, saveDir) {
             if(err) {
                 throw err.message
             }
-            console.log("Seed file Created")
+            console.log("JSON file Created")
         });
     });
 }
